a7e079a291f707392a4dbbf65600d9f61c3744cd
Johan Kiviniemi <devel@johan.kiviniemi.name>
2023-03-02 15:28:24 +0200

    generate-metalink: Switch to @xmcl/modrinth
    It supports Modrinth API v2.

--- generate-metalink/src/sources/modrinth.ts
+++ generate-metalink/src/sources/modrinth.ts
@@ -1,4 +1,4 @@
-import { Modrinth } from "modrinth";
+import { getProject, getProjectVersions } from "@xmcl/modrinth";
 
 import { Download } from "../types/download";
 
@@ -6,16 +6,15 @@ export async function modrinthDownload(
   minecraftVersion: string,
   modId: string
 ): Promise<Download> {
-  const modrinth = new Modrinth();
-  const mod = await modrinth.mod(modId);
-  const versions = await mod.versions();
+  const mod = await getProject(modId);
+  const versions = await getProjectVersions(modId);
   const candidates = versions.filter(
     (ver) =>
-      (ver.type === "release" || ver.type === "beta") &&
-      ((ver.game_versions as string[]).includes(minecraftVersion) ||
+      (ver.version_type === "release" || ver.version_type === "beta") &&
+      (ver.game_versions.includes(minecraftVersion) ||
         // TODO: workaround for MapModCompanion
-        (ver.game_versions as string[]).includes("1.19.2")) &&
-      (ver.loaders as string[]).includes("paper")
+        ver.game_versions.includes("1.19.2")) &&
+      ver.loaders.includes("paper")
   );
   if (candidates.length === 0) {
     throw new Error(
@@ -27,15 +26,25 @@ export async function modrinthDownload(
     versionA.date_published > versionB.date_published ? versionA : versionB
   );
 
-  const file = version.files.find((f) => f.primary);
-  if (!file)
+  const file = version.files.find((f) => (f as { primary?: boolean }).primary);
+  if (!file) {
     throw new Error(
-      `Failed to find file information for ${mod.title} (${mod.id})`
+      `Failed to find file information for ${mod.title} (${mod.id})` +
+        `, got ${JSON.stringify(version.files)}`
     );
+  }
+
+  const { sha512 } = file.hashes;
+  if (!sha512) {
+    throw new Error(
+      `Failed to find a SHA-512 hash for ${mod.title} (${mod.id})` +
+        `, got ${JSON.stringify(file.hashes)}`
+    );
+  }
 
   return new Download({
     url: new URL(file.url),
-    checksum: { type: "sha-512", hash: file.hashes.sha512 },
+    checksum: { type: "sha-512", hash: sha512 },
   });
 }
 
